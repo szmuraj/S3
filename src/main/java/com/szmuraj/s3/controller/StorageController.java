38ba63b62c0c9bf5dc9a5cd1ef05bb4bf3b4872c
szmuraj <przemyslaw.sicinski@o2.pl>
2023-07-07 10:42:23 +0200

    a

--- src/main/java/com/szmuraj/s3/controller/StorageController.java
+++ src/main/java/com/szmuraj/s3/controller/StorageController.java
@@ -1,2 +1,40 @@
-package com.szmuraj.s3.controller;public class StorageController {
+package com.szmuraj.s3.controller;
+
+import com.szmuraj.s3.service.StorageService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/file")
+public class StorageController {
+
+    private final StorageService service;
+
+    public StorageController(StorageService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
 }
